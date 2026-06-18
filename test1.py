142b0fe77db684d398938eb06e4ae2909bd9306f
Haura Agnia Ramadhani A <hauraamnun@gmail.com>
2026-06-18 11:36:46 +0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -10,4 +10,16 @@ for i in range(1, 8):  # untuk n =7, range adalah limitasi
 # output
 print("Hasil:", total_sum) #hasil akhir semua penjumlahan
 
+# Cara 2 Untuk melihat hasil secara bertahap
+total_sum = 0
+
+# Perhitungan menggunakan metode perulangan
+for i in range(1, 8):  # untuk n=7
+    rumus = 1 / (3*i) #masukan rumus notasi sigma
+    total_sum += rumus #total awal akan ditambah dengan hasil rumus secara berkala
+    print(f" untuk i = {i}, adalah = {rumus:.4f}, total= {total_sum:.4f}") #menampilkan hasil penjumlahan secara berkala
+
+# Output
+print("Total akhir:", total_sum) #hasil akhir total penjumlahan
+
 #JANGAN LUPA HITUNG MANUAL DI BUKU TUGAS UNTUK PEMBUKTIAN
